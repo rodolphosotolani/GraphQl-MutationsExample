476173dbac7f31d36e1fc607d3da3c3c890a616f
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-16 21:59:18 -0400

    feature: Create a new Project

---
+++ src/main/java/br/com/rts/estudos/graphql/mutations/MutationsExamplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.mutations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutationsExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutationsExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rts/estudos/graphql/mutations/MutationsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.mutations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutationsExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
